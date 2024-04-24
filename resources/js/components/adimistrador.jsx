9d09376bfdce6ddc361f319d1e1737815657cffb
anzambrano21 <lolepico1234@gmail.com>
2024-04-24 09:28:10 -0400

    creacion de archivo de los componentes admin

---
+++ resources/js/components/adimistrador.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../../css/app.css'
+const itemsPerPage = 5;
+let res = await fetch('http://127.0.0.1:8000/api/usuario');
+let myData = await res.json();
+let data2 = myData;
+const fetchData = async () => {
+  res= await fetch('http://127.0.0.1:8000/api/usuario');
+  myData = await res.json();
+  data2=myData
+
+};
+
+export const adimistrador=()=>{
+    return (
+        <div className='container'>
+          <div className='nav'>
+
+
+
+            
+
+            <div className="footer">
+
+            </div>
+
+            
+          </div>
+
+        
+        
+        
+        </div>
+    );
+}
