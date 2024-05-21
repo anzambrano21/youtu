ef73affd9c55f9cef241f47ac558b9d716450bcf
anzambrano21 <lolepico1234@gmail.com>
2024-05-21 17:03:19 -0400

    carga de foto de perfil

--- resources/js/components/usuario.jsx
+++ resources/js/components/usuario.jsx
@@ -1,53 +1,86 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import React, { useContext } from 'react';
+import React, { useContext, useState, useCallback } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 import "../../css/datuser.css"
+import { useDropzone } from "react-dropzone";
 
 import { Exaplecontect } from "../context/contexto";
 
 export const Cambiardat = () => {
     const example = useContext(Exaplecontect)
-    console.log(example);
-    function admin(){
-      if (example["datos"]["rol"]===1) {
-          return (      
-          <Typography variant="h6" >
-          <Link className="nav-link" id="tab-register" data-mdb-pill-init to="/admin" role="tab"
-            aria-controls="pills-register" aria-selected="false" >admin</Link>
-        </Typography>)
-  
-  
-      }
+    
+    const [files, setFiles] = useState([]);
+    const [isImageLoaded, setIsImageLoaded] = useState(false); // Nuevo estado
+
+    const onDrop = useCallback((acceptedFiles) => {
+        setFiles(
+            acceptedFiles.map((file) =>
+                Object.assign(file, {
+                    preview: URL.createObjectURL(file),
+                })
+            )
+        );
+        setIsImageLoaded(true); // Establece el estado a verdadero cuando se carga una imagen
+    }, []);
+
+    const { getRootProps, getInputProps } = useDropzone({ 
+        onDrop,
+        onDropAccepted: () => {
+            onDrop();
+            setFiles([]); // Limpia el estado de los archivos después de agregar el nuevo
+        },
+        maxFiles: 1, // Acepta solo un archivo
+        onDropRejected: () => alert('Por favor, sube solo imágenes.'), // Alerta si el archivo no es una imagen
+    });
+
+    const fileList = files.map((file) => (
+        
+            <img src={file.preview} alt={file.name} className="img-redonda" />
+        
+    ));
+
+    function admin() {
+        if (example["datos"]["rol"] === 1) {
+            return (
+                <Typography variant="h6" >
+                    <Link className="nav-link" id="tab-register" data-mdb-pill-init to="/admin" role="tab"
+                        aria-controls="pills-register" aria-selected="false" >admin</Link>
+                </Typography>)
+        }
     }
     return (
         <div className="datosUsuser">
-     <AppBar position="static" style={{ backgroundColor: '#EAEA12' }}>
-        <Toolbar className='nav'>
-          <Typography variant="h5">
-            Youtube Amarillo
-          </Typography>
-          <div className='items'>
-            <Typography variant="h6" >
-              <Link className="nav-link" id="tab-register" data-mdb-pill-init to="/home" role="tab"
-                aria-controls="pills-register" aria-selected="false" >Home</Link>
-            </Typography>
-            {admin()}
+            <AppBar position="static" style={{ backgroundColor: '#EAEA12' }}>
+                <Toolbar className='nav'>
+                    <Typography variant="h5">
+                        Youtube Amarillo
+                    </Typography>
+                    <div className='items'>
+                        <Typography variant="h6" >
+                            <Link className="nav-link" id="tab-register" data-mdb-pill-init to="/home" role="tab"
+                                aria-controls="pills-register" aria-selected="false" >Home</Link>
+                        </Typography>
+                        {admin()}
 
-          </div>
+                    </div>
 
 
-        </Toolbar>
-      </AppBar>
+                </Toolbar>
+            </AppBar>
 
 
             <div className="containe">
                 <div className='nombreap'>
                     <div className="row">{/* foto nombre apellido cedula */}
                         <div className="col">
-
+                            <div {...getRootProps()}>
+                                <input {...getInputProps()} />
+                                {!isImageLoaded && <p>Arrastra y suelta tus archivos aquí, o haz clic para seleccionar archivos</p>}
+                                <ul>{fileList}</ul>
+                            </div>
                         </div>
 
                         <div className="col">
@@ -73,31 +106,52 @@ export const Cambiardat = () => {
                     </div>
                 </div>
                 <div className='ubicacion'>
-                <div className="row gy-5">{/*correo rif direccion y estado  */}
-                    <p className="col-1">Correo</p>
-                    <input type="text" className="col" />
-                    <p className="col-1">RIF</p>
-                    <input type="text" className="col" />
-                    <p className="col-2">Direccion</p>
-                    <input type="text" className="col" />
+
+                    <div className="row">
+                        <p className="col-2">Correo</p>
+                        <input type="text" className="col" />
+                    </div>
+                    <div className="row">
+                        <p className="col-2">RIF</p>
+                        <input type="text" className="col" />
+                    </div>
+                    <div className="row">
+                        <p className="col-2">Direccion</p>
+                        <input type="text" className="col" />
+                    </div>
+                    <div className="row">
+                        <p className='col-2'>contraseña actual</p>
+                        <input type="text" className='col' />
+
+                    </div>
+                    <div className="row">
+                        <p className='col-2'>contraseña nueva</p>
+                        <input type="text" className='col' />
+                    </div>
+                    <div className="row">
+                        <select name="estado" id="" className='col-2' style={{ marginRight: "0.5%" }}>
+                            <option value="Carabobo">Carabobo</option>
+                            <option value="Amazonas">Amazonas</option>
+                            <option value="Anzoátegui">Anzoátegui</option>
+                            <option value="Apure">Apure</option>
+                            <option value="Aragua">Aragua</option>
+                            <option value="Bolívar">Bolívar</option>
+                            <option value="Barinas">Barinas</option>
+                            <option value="Cojedes">Cojedes</option>
+                            <option value="Delta Amacuro">Delta Amacuro</option>
+                            <option value="Distrito Capital	">Distrito Capital	</option>
+                            <option value="Falcón">Falcón</option>
+                            <option value="Guárico">Guárico	</option>
+                            <option value="Lara">Lara</option>
+                            <option value="Mérida">Mérida</option>
+                        </select>
+                        <input type="button" value="guardar" className='col-2' />
+                    </div>
+
 
                 </div>
-                <div className="row gy-5" >
-                    <p className='col-2'>contraseña actual</p>
-                    <input type="text" className='col' />
-                    
-                    <p className='col-2'>contraseña nueva</p>
-                    <input type="text" className='col' />
 
 
-                </div>
-                <div className="row">
-                    <select name="" id="" className='col-2' style={{ marginRight: "0.5%" }}></select>
-                    <input type="button" value="guardar" className='col-2' />
-                </div>
-                </div>
-
-                
             </div>
 
         </div>
